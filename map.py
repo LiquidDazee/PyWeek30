7093a4ee7518df1aa27a1611b7d0d1a1fc0b4280
Archit <archit10.1999@gmail.com>
2020-09-22 00:32:25 +0530

    OP walls

---
+++ map.py
@@ -0,0 +1,35 @@
+from kivy.graphics import Rectangle
+from settings import TILESIZE, WIDTH, HEIGHT
+
+
+class Map:
+    def __init__(self, filename):
+        self.data = []
+        with open(filename, 'rt') as f:
+            for line in f:
+                self.data.append(line.strip())
+
+        self.tilewidth = len(self.data[0])
+        self.tileheight = len(self.data)
+        self.width = self.tilewidth * TILESIZE
+        self.height = self.tileheight * TILESIZE
+
+class Camera:
+    def __init__(self, width, height):
+        self.camera = Rectangle(pos = (0,0), size = (width, height))
+        self.width = width
+        self.height = height
+
+    def apply(self, entity):
+        return entity.rect.move(self.camera.topleft)
+
+    def update(self, target):
+        x = -target.rect.x + int(WIDTH / 2)
+        y = -target.rect.y + int(HEIGHT / 2)
+
+        # limit scrolling to map size
+        x = min(0, x)  # left
+        y = min(0, y)  # top
+        x = max(-(self.width - WIDTH), x)  # right
+        y = max(-(self.height - HEIGHT), y)  # bottom
+        self.camera =  Rectangle(pos = (x,y), size = (self.width, self.height))
