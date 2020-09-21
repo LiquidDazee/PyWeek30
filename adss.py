ae91b90ae8952b9a917dcdeec2a383e9a44e66f1
Archit <archit10.1999@gmail.com>
2020-09-21 23:16:25 +0530

    OOP

---
+++ adss.py
@@ -0,0 +1,143 @@
+# import os
+# os.environ['KIVY_GL_BACKEND'] = 'sdl2'
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.uix.label import CoreLabel
+
+
+class GameWidget(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._keyboard = Window.request_keyboard(
+        self._on_keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_key_down)
+        self._keyboard.bind(on_key_up=self._on_key_up)
+
+    
+        self.register_event_type("on_frame")
+
+        with self.canvas:
+            Rectangle(source = "assets/bg.png", pos=(0, 0),
+                      size=(Window.width, Window.height))
+            
+    
+        self.keysPressed = set()
+        self._entities = set()
+
+        Clock.schedule_interval(self._on_frame, 0)
+        
+
+    def _on_frame(self, dt):
+        self.dispatch("on_frame", dt)
+
+    def on_frame(self, dt):
+        pass
+
+
+    def add_entity(self, entity):
+        self._entities.add(entity)
+        self.canvas.add(entity._instruction)
+
+    def remove_entity(self, entity):
+        if entity in self._entities:
+            self._entities.remove(entity)
+            self.canvas.remove(entity._instruction)
+
+
+    def _on_keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_key_down)
+        self._keyboard.unbind(on_key_up=self._on_key_up)
+        self._keyboard = None
+
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        self.keysPressed.add(keycode[1])
+
+    def _on_key_up(self, keyboard, keycode):
+        text = keycode[1]
+        if text in self.keysPressed:
+            self.keysPressed.remove(text)
+
+class Entity():
+    def __init__(self):
+        self._pos = (0, 0)
+        self._size = (50, 50)
+        self._source = ""
+        self._instruction = Rectangle(
+            pos=self._pos, size=self._size, source=self._source) 
+
+    @property
+    def pos(self):
+        return self._pos
+
+    @pos.setter
+    def pos(self, value):
+        self._pos = value
+        self._instruction.pos = self._pos
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._size = value
+        self._instruction.size = self._size
+
+    @property
+    def source(self):
+        return self._source
+
+    @source.setter
+    def source(self, value):
+        self._source = value
+        self._instruction.source = self._source
+
+
+
+class Player(Entity):
+    def __init__(self):
+        super().__init__()
+        self.source = "assets/sprite.png"
+        game.bind(on_frame=self.move_step)
+
+    def move_step(self, sender, dt):
+        currentx = self.pos[0]
+        currenty = self.pos[1]
+        oldpos = (currentx, currenty)
+        newx = currentx
+        newy = currenty
+
+        step_size = 300*dt
+
+        if "w" in game.keysPressed and newy<game.height - 50:
+            newy += step_size
+        if "s" in game.keysPressed and newy>0:
+            newy -= step_size
+        if "a" in game.keysPressed and newx>0:
+            newx -= step_size
+        if "d" in game.keysPressed and newx<game.width - 50:
+            newx += step_size
+        # self.player.pos = (newx, newy)
+        self.pos = (newx, newy)
+        # if "w" in self.keysPressed or "d" in self.keysPressed or "a" in self.keysPressed or "s" in self.keysPressed:
+        
+
+        
+
+game = GameWidget()
+game.player = Player()
+game.player.pos = (Window.width/2  , Window.height/2)
+game.add_entity(game.player)
+
+class MyApp(App):
+    def build(self):
+        return game
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.run()
